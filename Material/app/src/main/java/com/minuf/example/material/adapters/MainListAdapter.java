45c502109698203c92dc127fa877c2addfc7eeae
minuf <minuel.jc@gmail.com>
2015-08-26 15:45:22 +0200

    backup with picasso library

--- Material/app/src/main/java/com/minuf/example/material/adapters/MainListAdapter.java
+++ Material/app/src/main/java/com/minuf/example/material/adapters/MainListAdapter.java
@@ -1,5 +1,6 @@
 package com.minuf.example.material.adapters;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import com.minuf.example.material.R;
 import com.minuf.example.material.items_struc.ItemList1_Structure;
 import com.minuf.example.material.views.RoundedImageView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -21,6 +23,7 @@ import java.util.ArrayList;
 public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ItemViewHolder> {
 
     private ArrayList<ItemList1_Structure> arrayData;
+    private static Context context;
 
     //constructor
     public MainListAdapter(ArrayList<ItemList1_Structure> arrayData) {
@@ -36,7 +39,11 @@ public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ItemVi
 
         ItemViewHolder holder = new ItemViewHolder(itemView);
 
+        context = viewGroup.getContext();
+
         return holder;
+
+
     }
 
     @Override
@@ -73,7 +80,14 @@ public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ItemVi
 
             iv_image.setImageBitmap(icon);*/
 
-            iv_image.setImageResource(item.getImage());
+            //iv_image.setImageResource(item.getImage());
+
+
+            //CON IMAGENES DEMASIADO GRANDES LANZA UN ERROR EN LA CLASE ROUNDEDIMAGEVIEW...algun conflicto al convertir a bitmap
+            Picasso.with(context)
+                    .load("http://blog.dictionary.com/wp-content/uploads/2013/11/selfie_big.jpg")
+                    .into(iv_image);
+
             tv_title.setText(item.getTitle());
             tv_subtitle.setText(item.getSubtitle());
         }
