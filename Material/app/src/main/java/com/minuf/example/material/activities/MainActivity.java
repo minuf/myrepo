c8a10e9e6109b31605577dc383a629f39056eb63
minuf <minuel.jc@gmail.com>
2015-08-28 04:04:26 +0200

    added transitions between activities

--- Material/app/src/main/java/com/minuf/example/material/activities/MainActivity.java
+++ Material/app/src/main/java/com/minuf/example/material/activities/MainActivity.java
@@ -5,8 +5,8 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.design.internal.ScrimInsetsFrameLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,7 +15,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +23,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.minuf.example.material.R;
 import com.minuf.example.material.adapters.FragmentsAdapter;
@@ -162,7 +162,6 @@ public class MainActivity extends AppCompatActivity {
 
                     ActivityOptions options = ActivityOptions
                             .makeSceneTransitionAnimation(MainActivity.this, v, v.getTransitionName());
-
 
                     startActivity(intent, options.toBundle());
 
